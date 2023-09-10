21718c93965ae7808474a440a5c500cfde8060e2
Yasin Karasu <darestar1@users.noreply.github.com>
2023-09-11 01:07:58 +0300

    database

--- carzone/settings.py
+++ carzone/settings.py
@@ -107,25 +107,25 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'car_db',
-         'USER': 'postgres',
-         'PASSWORD': 'fener&1907',
-         'HOST': '127.0.0.1',
-         'PORT': '5432',
-     }
- }
+# DATABASES = {
+#      'default': {         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#          'NAME': 'car_db',
+#          'USER': 'postgres',
+#          'PASSWORD': 'fener&1907',
+#          'HOST': '127.0.0.1',
+#          'PORT': '5432',
+#      }
+#  }
 
 
 
 # DATABASES = {'default': dj_database_url.config(default='postgres://postgres:fener&1907@localhost/car_db')}
-# DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
