d661f4e920f058d5079b325c49cc8187f0516b41
Yasin Karasu <darestar1@users.noreply.github.com>
2023-09-11 00:44:32 +0300

    database

--- carzone/settings.py
+++ carzone/settings.py
@@ -118,13 +118,13 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 
 
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:fener&1907@localhost/car_db')}
-# DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:fener&1907@localhost/car_db')}
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
